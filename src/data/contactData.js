946828292f5ef98a5861f03d970a77c85b562b43
Guido Suarez <guido.suarez.rossi@gmail.com>
2026-02-17 21:17:39 -0300

    vercel-final

--- src/data/contactData.js
+++ src/data/contactData.js
@@ -3,7 +3,7 @@ const contacts = [
     id: 1,
     name: "Yoda",
     last_time_conection: "Hace 2 horas",
-    profile_picture:
+    porfile_picture:
       "https://images.immediate.co.uk/production/volatile/sites/3/2017/12/yoda-the-empire-strikes-back-28a7558.jpg?quality=90&webp=true&resize=800,534",
 
     messages: [
